e93464ddfef05250649933ebad36263dc1fb7e8e
SeaotterMS <peter910820@gmail.com>
2026-01-08 23:25:44 +0800

    feat: add proxy

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.24.0
 require github.com/PuerkitoBio/goquery v1.11.0
 
 require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.3 // indirect
-	golang.org/x/net v0.47.0 // indirect
+	github.com/kuro-helper/kurohelper-proxy v1.0.0
+	golang.org/x/net v0.48.0 // indirect
 )
--- letao/request.go
+++ letao/request.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"time"
+
+	kurohelperproxy "github.com/kuro-helper/kurohelper-proxy"
 )
 
 func sendGetRequest(url string) (io.Reader, error) {
@@ -15,13 +19,22 @@ func sendGetRequest(url string) (io.Reader, error) {
 
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0")
 
-	client := &http.Client{}
+	dialer, err := kurohelperproxy.GetProxyDialer(os.Getenv("PROXY_PRIVATE_IP"), nil, os.Getenv("PROXY_PRIVATE_PORT"))
+	if err != nil {
+		fmt.Print(err)
+		return nil, err
+	}
+	client := &http.Client{
+		Transport: &http.Transport{
+			Dial: dialer.Dial,
+		},
+		Timeout: 10 * time.Second,
+	}
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
-
-	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
 		return nil, fmt.Errorf("kurobidder: server return a error code %d", resp.StatusCode)
